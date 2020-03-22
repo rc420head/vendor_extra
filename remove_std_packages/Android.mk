85a7137cd62f1098a93e3fcd7d45e46b70bab441
rc420head <dennislebhardt79@gmail.com>
2020-03-22 16:08:34 -0500

    use special Gcam for OnePlus 7pro

--- remove_std_packages/Android.mk
+++ remove_std_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemoveStdPackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Calendar DeskClock MusicFX Snap Camera2 Jelly Browser VanillaMusic Contacts Messaging
+LOCAL_OVERRIDES_PACKAGES := Calendar DeskClock MusicFX Jelly Browser VanillaMusic Contacts Messaging
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
