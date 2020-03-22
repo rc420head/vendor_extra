f7948140a32700a4daa744afd5a6aae88ae6e905
rc420head <dennislebhardt79@gmail.com>
2020-03-22 17:02:23 -0500

    Derp

--- product.mk
+++ product.mk
@@ -2,6 +2,12 @@ ifeq ($(filter h870 h872 us997,$(TARGET_DEVICE)),)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 endif
 
-# Remove unwanted packages
 PRODUCT_PACKAGES += \
-    RemoveStdPackages
+    RemoveStdPackages \
+    Nova
+
+ifneq ($(filter guacamole, $(TARGET_DEVICE)),)
+PRODUCT_PACKAGES += \
+    OP7Gcam
+endif
+
