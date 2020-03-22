fc34caddc607eab66329beaf341d43eb920d54ff
rc420head <dennislebhardt79@gmail.com>
2020-03-22 15:59:05 -0500

    filter out LG G6 from gapps

--- product.mk
+++ product.mk
@@ -1,4 +1,7 @@
+ifeq ($(filter h870 h872 us997,$(TARGET_DEVICE)),)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+endif
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemoveStdPackages
