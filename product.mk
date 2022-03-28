86c1203e6273196b5e7daa7c692b2952b179843e
rc420head <dennislebhardt79@gmail.com>
2022-03-28 00:06:32 -0500

    just gapps for now
    Change-Id: I8bec9fb718e46dffba46f023faa7b4d7768bf07c

--- product.mk
+++ product.mk
@@ -1,14 +1,14 @@
-$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
 
-PRODUCT_PACKAGES += \
-    RemoveStdPackages
+#PRODUCT_PACKAGES += \
+#    RemoveStdPackages
 
 # log privapp-permissions whitelist
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=log
+#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#    ro.control_privapp_permissions=log
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.disable_rescue=true \
-    persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0 \
-    ro.build.selinux=1
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    persist.sys.disable_rescue=true \
+#    persist.debug.wfd.enable=1 \
+#    persist.sys.wfd.virtual=0 \
+#    ro.build.selinux=1
