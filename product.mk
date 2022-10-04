53882a0cbc113e81f01f61adc1407c86b3d751d9
Dennis S Lebhardt jr <dennislebhardt79@gmail.com>
2022-10-04 18:02:10 -0500

    update for 20.0

--- product.mk
+++ product.mk
@@ -1,14 +1,4 @@
-$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 
 PRODUCT_PACKAGES += \
     RemoveStdPackages
-
-# log privapp-permissions whitelist
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=log
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.disable_rescue=true \
-    persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0 \
-    ro.build.selinux=1
