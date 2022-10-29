006d6a987512883fbd58ddc8c6fadddd86ae7ecd
rc420head <dennislebhardt79@gmail.com>
2022-10-28 21:48:24 -0500

    remove a few more packages

--- remove_std_packages/Android.mk
+++ remove_std_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemoveStdPackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := DeskClock Etar Jelly Eleven Contacts messaging Dialer Email
+LOCAL_OVERRIDES_PACKAGES := DeskClock Etar Jelly Eleven messaging Dialer Email Calendar webview Home Launcher2 Launcher3 Launcher3Go QuickSearchBox TrebuchetGo
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
